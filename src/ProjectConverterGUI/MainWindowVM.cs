6e702fedacffa28e9fa1213cd5d72ae99dda2eb7
SchreinerK <ks@ksware.de>
2021-08-19 17:16:42 +0200

    Add project files.

---
+++ src/ProjectConverterGUI/MainWindowVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KsWare.Presentation.Lite;
+
+namespace ProjectConverterGUI {
+
+	public class MainWindowVM : NotifyPropertyChangedBase {
+
+		private string _inputPath;
+
+		public string InputPath { get => _inputPath; set => Set(ref _inputPath, value); }
+	}
+}
